58b70f7956a8f8a7803fc67eb7f64cf2325d0a83
BlumaDeutsch <blimy1219@gmail.com>
2025-04-29 16:57:10 +0300

    merge 3 files into 1 file as required

--- main.py
+++ main.py
@@ -126,34 +126,37 @@ def gauss_seidel_method(matrix, vector, epsilon=0.00001, max_iterations=100):
 
 def main():
     print(" Start! The code has started running")
+    try:
+        # הגדרת המטריצה והוקטור
+        matrix = [[4, 2, 0], [2, 10, 4], [0, 4, 5]]
+        vector = [[2], [6], [5]]
 
-    # הגדרת המטריצה והוקטור
-    matrix = [[4, 2, 0], [2, 10, 4], [0, 4, 5]]
-    vector = [[2], [6], [5]]
+        # הדפסת מערכת המשוואות
+        print("📘 The system of equations has been defined.")
+        for row in matrix:
+            print(row)
+        print("📘 vector B:", vector)
 
-    # הדפסת מערכת המשוואות
-    print("📘 The system of equations has been defined.")
-    for row in matrix:
-        print(row)
-    print("📘 vector B:", vector)
+        # קלט מהמשתמש – איזו שיטה לבחור
+        method = input("🔢 Choose a method (jacobi / gauss):\n>> ").strip().lower()
 
-    # קלט מהמשתמש – איזו שיטה לבחור
-    method = input("🔢 Choose a method (jacobi / gauss):\n>> ").strip().lower()
+        if method == "jacobi":
+            print("\n Solution using the Jacobi method:")
+            solution = jacobi_method(matrix, vector)
+        elif method == "gauss":
+            print("\n Solution using the Gauss-Seidel method:")
+            solution = gauss_seidel_method(matrix, vector)
+        else:
+            solution = None
+            print("Unrecognized method, try again.")
 
-    if method == "jacobi":
-        print("\n Solution using the Jacobi method:")
-        solution = jacobi_method(matrix, vector)
-    elif method == "gauss":
-        print("\n Solution using the Gauss-Seidel method:")
-        solution = gauss_seidel_method(matrix, vector)
-    else:
-        solution = None
-        print("Unrecognized method, try again.")
-
-    if solution:
-        print(f"\n✅ Final solution: {solution}")
-    else:
-        print("❌ The method did not converge.")
+        if solution:
+            print(f"\n✅ Final solution: {solution}")
+        else:
+            print("❌ The method did not converge.")
+    except Exception as e:
+        print("\n<UNK> The method did not converge.")
+        print(e)
 
 if __name__== "__main__":
     main()
